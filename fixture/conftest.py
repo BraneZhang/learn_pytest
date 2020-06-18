e1d872f6c688a580c2035c6236e5391094587305
branezhang <branezhang@163.com>
2020-06-18 17:35:52 +0800

    4.1

--- fixture/conftest.py
+++ fixture/conftest.py
@@ -6,3 +6,15 @@ import pytest
 @pytest.fixture()
 def login():
     print("输入账号，密码先登录")
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--cmdopt", action="store", default="type1", help="my option: type1 or type2"
+    )
+
+
+@pytest.fixture
+def cmdopt(request):
+    # 获取pytest_addoption中的值
+    return request.config.getoption("--cmdopt")
